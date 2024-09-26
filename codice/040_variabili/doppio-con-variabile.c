373d43f5b9d0042bfe6a371ed823291637c34585
Marco Alberti <malbertife@gmail.com>
2024-09-26 15:05:12 +0200

    doppio con variabile

---
+++ codice/040_variabili/doppio-con-variabile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // definisco la variabile intera v
+    int v;
+    int doppio;
+
+    // richiedo all'utente il valore di v (input)
+    scanf("%d", &v);
+    
+    doppio = v * 2;
+
+    // stampo il valore di v (output)
+    printf("%d\n", doppio);
+}
