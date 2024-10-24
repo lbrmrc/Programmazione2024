75c7463df51a78c59545a7a6157dd4c88267f450
Marco Alberti <marco.alberti@unife.it>
2024-10-24 15:35:13 +0200

    giorno della settimana

--- codice/l04/giorno-della-settimana.c
+++ codice/l04/giorno-della-settimana.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int giorno_giuliano(int g, int m, int a) {
   int N0, N1, N2, N3;
   N0 = (m - 14) / 12;
@@ -6,3 +8,23 @@ int giorno_giuliano(int g, int m, int a) {
   N3 = 3 * (a + 4900 + N0) / 400;
   return N1 + N2 - N3 + g - 32075;
 }
+
+char giorno (int giorno, int mese, int anno){
+  switch (giorno_giuliano(giorno, mese, anno) % 7){
+    case 0: return 'l';
+    case 1: return 'm';
+    case 2: return 'M';
+    case 3: return 'g';
+    case 4: return 'v';
+    case 5: return 's';
+    case 6: return 'd';
+  }
+}
+
+int main(){
+  int G,M,A;
+  printf("Inserisci una data (giorno, mese, anno)\n");
+  scanf("%d%d%d",&G,&M,&A);
+  printf("%c\n", giorno(G,M,A));
+  return 0;
+} 
